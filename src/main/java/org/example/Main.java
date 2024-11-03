74c3f65066a5b080f588bfbc8d58019f3355c373
Muhammadkomil <muhammadkomil.realsoft@gmail.com>
2024-11-03 15:40:49 +0500

    powered by Muhammadkomil

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author Muhammadkomil Murodillayev
+ * @created 03/11/2024 - 15:37
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
